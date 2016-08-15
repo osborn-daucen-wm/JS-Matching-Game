82bd3fbcf459affea9a0407f23b5c72ad2d1d2a2
daucen <daucen.osborn@west-mec.org>
2016-08-15 15:27:34 -0700

    Part 3 stuff.

--- scripts3.js
+++ scripts3.js
@@ -1,3 +1,19 @@
-/**
- * Created by session1 on 8/15/16.
- */
+var numberOfCookies = 5;
+var leftSide = document.getElementById("leftSide");
+var rightSide = document.getElementById("rightSide");
+
+function generateCookies() {
+    var count = 0;
+    while (count <= numberOfCookies) {
+        var cookies = document.createElement("img");
+        cookies.src = "cookie.png";
+        cookies.style.top = Math.random() * 400 + "px";
+        cookies.style.left = Math.random() * 400 + "px";
+        leftSide.appendChild(cookies);
+        count++;
+    }
+}
+
+leftSideImages = theLeftSide.cloneNode(true);
+
+generateCookies();
